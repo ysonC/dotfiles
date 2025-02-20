f106108c18b9368015d0539045e95221b8e92589
Wyson Cheng <wyson002@gmail.com>
2025-02-20 22:07:40 +0000

    add completion

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,50 +1,52 @@
-return{
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",   --Lua
-                    "pyright",  --Python
-                    "gopls",     --Go
-                    "yamlls"    --YAML
-                }
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls", --Lua
+					"pyright", --Python
+					"gopls", --Go
+					"yamlls", --YAML
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
+			-- Python Language Server
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-            -- Lua Language Server
-            lspconfig.lua_ls.setup({})
+			-- YAML
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+				settings = {
+					yaml = {
+						validate = true,
+						hover = true,
+						completion = true,
+					},
+				},
+			})
 
-            -- Python Language Server
-            lspconfig.pyright.setup({})
-
-            -- YAML
-            lspconfig.yamlls.setup({
-                settings = {
-                    yaml = {
-                        validate = true,
-                        hover = true,
-                        completion = true,
-                    },
-                },
-            })
-
-
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
