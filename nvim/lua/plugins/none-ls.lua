5979bf4dc36f94ee55f1e78e0e3687fdd09179b3
Wyson Cheng <wyson002@gmail.com>
2025-02-23 20:49:45 +0000

    update

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,17 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.diagnostics.semgrep,
-			},
-		})
+            },
+        })
 
-		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+    end,
 }
