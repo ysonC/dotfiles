76a28061f8c6ed6fc09f98e6ebd213e4491439a5
Wyson Cheng <wyson002@gmail.com>
2025-02-28 22:43:09 +0000

    add go lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,87 +1,106 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls", --Lua
-                    "pyright", --Python
-                    "gopls", --Go
-                    "yamlls", --YAML
-                    "ts_ls", --TypeScript
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls", --Lua
+					"pyright", --Python
+					"gopls", --Go
+					"yamlls", --YAML
+					"ts_ls", --TypeScript
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- Lua
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			-- Lua
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            -- Python Language Server
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    pyright = { autoImportCompletion = true },
-                    python = {
-                        analysis = {
-                            autoSearchPaths = true,
-                            diagnosticMode = "openFilesOnly",
-                            useLibraryCodeForTypes = true,
-                            typeCheckingMode = "off",
-                        },
-                    },
-                },
-            })
+			-- Python Language Server
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					pyright = { autoImportCompletion = true },
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							diagnosticMode = "openFilesOnly",
+							useLibraryCodeForTypes = true,
+							typeCheckingMode = "off",
+						},
+					},
+				},
+			})
 
-            -- TypeScript
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-                on_attach = function(client)
-                    client.server_capabilities.documentFormatingProvider = false
-                end,
-                settings = {
-                    javascript = {
-                        suggest = {
-                            autoImports = true,
-                        },
-                    },
-                    typescript = {
-                        suggest = {
-                            autoImports = true,
-                        },
-                    },
-                },
-            })
+			-- TypeScript
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = function(client)
+					client.server_capabilities.documentFormatingProvider = false
+				end,
+				settings = {
+					javascript = {
+						suggest = {
+							autoImports = true,
+						},
+					},
+					typescript = {
+						suggest = {
+							autoImports = true,
+						},
+					},
+				},
+			})
 
-            -- YAML
-            lspconfig.yamlls.setup({
-                capabilities = capabilities,
-                settings = {
-                    yaml = {
-                        validate = true,
-                        hover = true,
-                        completion = true,
-                    },
-                },
-            })
+			-- YAML
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+				settings = {
+					yaml = {
+						validate = true,
+						hover = true,
+						completion = true,
+					},
+				},
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			-- gopls
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = lspconfig.util.root_pattern("go.mod", ".git", "go.work"),
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+						staticcheck = true,
+					},
+				},
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
