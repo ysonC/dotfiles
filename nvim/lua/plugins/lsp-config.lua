606a8b7b62e7c239df1578c3b0ee0bf1186f4b6e
Wyson Cheng <wyson002@gmail.com>
2025-03-04 10:28:08 +0000

    new keymap

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -98,6 +98,7 @@ return {
 				},
 			})
 
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
