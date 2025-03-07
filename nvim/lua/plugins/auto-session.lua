3018a90f6b3096de9f6f9e54310d7507d5b26ca0
Wyson Cheng <wyson002@gmail.com>
2025-03-07 16:24:19 +0000

    update

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,11 +1,11 @@
 return {
-    "rmagatti/auto-session",
-    config = function()
-        local auto_session = require("auto-session")
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
 
-        auto_session.setup({
-            auto_restore_enabled = true,
-            auto_session_supress_dirs = { "~/" },
-        })
-    end,
+		auto_session.setup({
+			auto_restore_enabled = true,
+			auto_session_supress_dirs = { "~/" },
+		})
+	end,
 }
