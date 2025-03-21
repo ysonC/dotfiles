982835d507957f9cf1ce2b86f851b00589d173e6
Wyson Cheng <wyson002@gmail.com>
2025-03-21 13:01:26 +0000

    chore(update): update

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,13 +1,14 @@
 return {
-	"akinsho/nvim-bufferline.lua",
-	dependences = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	version = "*",
-	opts = {
-		options = {
-			mode = "tabs",
-			separator_style = "slant",
-		},
-	},
+    "akinsho/nvim-bufferline.lua",
+    dependences = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    version = "*",
+    opts = {
+        options = {
+            mode = "tabs",
+            separator_style = "slant",
+            auto_toggle_bufferline = true,
+        },
+    },
 }
