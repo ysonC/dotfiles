86675cb0a02865ad45d5efa37fd62f69371a023f
Wyson Cheng <wyson002@gmail.com>
2025-04-01 12:23:15 +0100

    feat(keymap): new keymap

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -2,8 +2,8 @@ local map = vim.keymap.set
 
 -- Basic
 map("n", "<leader>q", ":q<CR>")
-map("i", "qq", "<ESC>")
-map("v", "qq", "<ESC>")
+map("i", "<leader><BS>", "<ESC>")
+map("v", "<leader><BS>", "<ESC>")
 map("n", "<leader>y", '"+y')
 map("n", "<leader>w", ":w<CR>")
 map("n", "<C-c>", 'ggVG"+y')
@@ -12,10 +12,11 @@ map("n", "<C-n>", "<cmd>Neotree toggle<CR>", { silent = true })
 map("n", "<A-n>", "<cmd>Neotree left<CR>", { silent = true })
 map("n", "<A-up>", ":m .-2<CR>", { desc = "Move line up" })
 map("n", "<A-down>", ":m .+1<CR>", { desc = "Move line down" })
+map("n", "<leader>o", "o<Esc>")
 
 -- LSP
 map("n", "<leader>cs", function()
-	require("user.additional-schemas").init()
+    require("user.additional-schemas").init()
 end, { desc = "Choose YAML schema" })
 map("n", "<leader>r", vim.lsp.buf.rename, {})
 map("n", "K", vim.lsp.buf.hover, {})
@@ -33,23 +34,23 @@ map("n", "<C-p>", builtin.find_files, {})
 map("n", "<leader>fg", builtin.live_grep, {})
 
 -- Tab
-map("n", "<TAB>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
+-- map("n", "<TAB>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+-- map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+-- map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
 
 -- Harpoon
 local harpoon = require("harpoon")
 vim.keymap.set("n", "<leader>a", function()
-	harpoon:list():add()
-	vim.api.nvim_exec_autocmds("User", { pattern = "HarpoonChanged" })
+    harpoon:list():add()
+    vim.api.nvim_exec_autocmds("User", { pattern = "HarpoonChanged" })
 end, { desc = "Harpoon: Add file" })
 vim.keymap.set("n", "<leader>m", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
+    harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 for i = 1, 5 do
-	vim.keymap.set("n", "<leader>" .. i, function()
-		harpoon:list():select(i)
-	end, { desc = "Harpoon: Go to file " .. i })
+    vim.keymap.set("n", "<leader>" .. i, function()
+        harpoon:list():select(i)
+    end, { desc = "Harpoon: Go to file " .. i })
 end
 
 -- Undotree
