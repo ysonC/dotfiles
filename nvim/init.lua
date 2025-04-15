a07e4ed43296a28cf59ca0d9d7a8307310771282
Wyson Cheng <wyson002@gmail.com>
2025-04-15 13:41:16 +0100

    chore(init): remove init notify

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,63 +14,53 @@ vim.opt.scrolloff = 10
 vim.opt.conceallevel = 2
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight yanked text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
-    end,
+	desc = "Highlight yanked text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
 })
 
 -- Set up scrolloff
 vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
-    group = vim.api.nvim_create_augroup("ScrollOffEOF", {}),
-    callback = function()
-        local win_h = vim.api.nvim_win_get_height(0)
-        local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
-        local dist = vim.fn.line("$") - vim.fn.line(".")
-        local rem = vim.fn.line("w$") - vim.fn.line("w0") + 1
-        if dist < off and win_h - rem + dist < off then
-            local view = vim.fn.winsaveview()
-            view.topline = view.topline + off - (win_h - rem + dist)
-            vim.fn.winrestview(view)
-        end
-    end,
+	group = vim.api.nvim_create_augroup("ScrollOffEOF", {}),
+	callback = function()
+		local win_h = vim.api.nvim_win_get_height(0)
+		local off = math.min(vim.o.scrolloff, math.floor(win_h / 2))
+		local dist = vim.fn.line("$") - vim.fn.line(".")
+		local rem = vim.fn.line("w$") - vim.fn.line("w0") + 1
+		if dist < off and win_h - rem + dist < off then
+			local view = vim.fn.winsaveview()
+			view.topline = view.topline + off - (win_h - rem + dist)
+			vim.fn.winrestview(view)
+		end
+	end,
 })
 
 -- Create a command "SetSchema" that calls our script:
 vim.api.nvim_create_user_command("SetSchema", function()
-    require("user.additional-schemas").init()
+	require("user.additional-schemas").init()
 end, {})
 
 -- Lazy loading
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-    change_detection = {
-        notify = false,
-    },
-})
-
-vim.api.nvim_create_autocmd("User", {
-    pattern = "VeryLazy",
-    callback = function()
-        vim.notify("✅ Neovim fully loaded!", 2, {
-            title = "Startup",
-            timeout = 100,
-        })
-    end,
+	change_detection = {
+		notify = false,
+	},
 })
 
 -- Set Keymaps
