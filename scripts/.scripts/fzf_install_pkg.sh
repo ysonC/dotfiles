827b5219983ae6ff9b90a27b4f7a50df513f78c1
Wyson Cheng <wyson002@gmail.com>
2025-11-10 11:09:45 +0000

    feat(keybind): add new shortcut to open installingpkg in a floating window

--- scripts/.scripts/fzf_install_pkg.sh
+++ scripts/.scripts/fzf_install_pkg.sh
@@ -2,6 +2,6 @@
 
 pkg=$(paru -Slq | fzf --multi --ansi \
     --preview 'paru -Si {1} 2>/dev/null' \
-    --preview-window=right:60%)
+    --preview-window=right:75%)
 
 [[ -n "$pkg" ]] && paru -S $pkg
