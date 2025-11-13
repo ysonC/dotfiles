d129f6ea575df5b1e5fabeabf334bbcf8ee8fdfa
Wyson Cheng <wyson002@gmail.com>
2025-11-13 17:11:22 +0000

    chore(install): update neovim install for apt

--- install/install-neovim.sh
+++ install/install-neovim.sh
@@ -9,7 +9,10 @@ log "Installing Neovim and related tooling"
 if using_brew || using_pacman; then
   install_packages neovim ripgrep fd
 else
-  install_packages neovim ripgrep fd-find
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+  sudo rm -rf /opt/nvim-linux-x86_64
+  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+  install_packages ripgrep fd-find
   ensure_fd_alias
 fi
 
