d430903880ea537f4ceabf0dcbb808cf3a071d4d
Wyson Cheng <wyson002@gmail.com>
2026-05-14 11:09:44 +0800

    fix(nvim): fix commenting issue on yamls

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -7,9 +7,18 @@ return {
 	config = function()
 		local comment = require("Comment")
 
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+		local ts_pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 		comment.setup({
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = function(ctx)
+				local ok, commentstring = pcall(ts_pre_hook, ctx)
+				if ok and commentstring then
+					return commentstring
+				end
+
+				if vim.bo.commentstring ~= "" then
+					return vim.bo.commentstring
+				end
+			end,
 		})
 	end,
 }
